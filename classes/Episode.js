96738c7bb5ad9b18d7a23035743880770a3310e4
Luke Henke <luop90@gmx.com>
2016-12-22 20:02:18 -0700

    Added Episode.beautify()

--- classes/Episode.js
+++ classes/Episode.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const moment = require('moment');
 const api = Electron.remote.require('../api.js');
 
 class Episode {
@@ -8,8 +9,27 @@ class Episode {
 
   }
 
-  beautifyEpisode() {
+  /**
+   * Beautifies all the episode variables.
+   * @param  {int} index The new index of the episode (set to the ID)
+   * @return {void}
+   */
+  beautify(index) {
+    let episode = this;
 
+    episode.pubDateParsed = moment(episode.pubDate).format('MMMM DD, YYYY');
+    episode.id = index;
+
+    // Sometimes the length is already parsed.
+    if (episode['itunes:duration']['#'].includes(':')) {
+      episode.podcastLengthParsed = episode['itunes:duration']['#'];
+    } else {
+      episode.podcastLengthParsed = api.formatSecondsToWords(episode['itunes:duration']['#']);
+    }
+
+    episode.podcastLength = api.formatSecondsToHoursMinutesSeconds(episode['itunes:duration']['#']);
+    episode.currentTime = 0;
+    episode.watched = false;
   }
 
   downloadEpisode() {
