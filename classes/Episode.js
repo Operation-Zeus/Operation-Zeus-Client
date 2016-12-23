cdd40919f152d14e4af62e7e2c3af78fb2cb6a94
Luke Henke <luop90@gmx.com>
2016-12-22 19:57:59 -0700

    Added skeletons

--- classes/Episode.js
+++ classes/Episode.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const fs = require('fs');
+const api = Electron.remote.require('../api.js');
+
 class Episode {
   constructor() {
+
+  }
+
+  beautifyEpisode() {
+
+  }
+
+  downloadEpisode() {
+
+  }
+
+  /**
+   * Deletes the episode in question
+   * @return {void}
+   */
+  deleteEpisode() {
 
   }
 }
--- classes/Podcast.js
+++ classes/Podcast.js
@@ -1,8 +1,31 @@
 'use strict';
 
+const request = require('request');
+const api = Electron.remote.require('../api.js');
+
 class Podcast {
   constructor() {
 
+  }
+
+  savePodcast() {
+
+  }
+
+  updatePodcast() {
+
+  }
+
+  removePodcast() {
+
+  }
+
+  updateCachedImage() {
+
+  }
+
+  updateSavedPodcast() {
+    
   }
 }
 
