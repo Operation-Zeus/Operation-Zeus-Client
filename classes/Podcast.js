9e301a54b3dbfc98d8906044d0b5c07816604b63
Luke Henke <luop90@gmx.com>
2016-12-25 10:55:58 -0700

    Added constructor, added addEpisode(), fixed require()s

--- classes/Podcast.js
+++ classes/Podcast.js
@@ -1,11 +1,44 @@
 'use strict';
 
 const request = require('request');
-const api = Electron.remote.require('../api.js');
+
+const api = require('../api.js');
+const Episode = require('./Episode.js');
 
 class Podcast {
-  constructor() {
+  /**
+   * Initializes the podcast
+   * @param  {String} url The URL of the RSS feed
+   * @return {void}
+   */
+  constructor(url) {
+    this.meta = {};
+    this.episodes = [];
+    this.rssURL = url;
+    this.loading = false;
+    this.imageURL = '';
+  }
 
+  /**
+   * Creates a new episode from the stream reader data
+   * @param {Object} data Stream reader information
+   * @return {void}
+   */
+  addEpisode(data) {
+    let episode = new Episode({
+      author: data.author || data['itunes:author']['#'],
+      id: this.episodes.length,
+      date: data.date,
+      description: data.description,
+      summary: data.summary,
+      imageURL: data['itunes:image']['@'].href,
+      source: data.link,
+      guid: data.guid,
+      length: data['itunes:duration']['#'],
+      tite: data.title
+    });
+
+    this.episodes.push(episode);
   }
 
   savePodcast() {
@@ -22,10 +55,6 @@ class Podcast {
 
   updateCachedImage() {
 
-  }
-
-  updateSavedPodcast() {
-    
   }
 }
 
