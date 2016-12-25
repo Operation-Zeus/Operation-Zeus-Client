616ba965230b77dd7ed6750ffd5719799d869645
Luke Henke <luop90@gmx.com>
2016-12-25 12:53:06 -0700

    Fixed angular not updating when using non-sync file operations, added a timeout before loading files

--- assets/src/js/app.run.js
+++ assets/src/js/app.run.js
@@ -5,34 +5,40 @@ angular
   .run(runBlock);
 
 /* @ngInject */
-function runBlock($window, $rootScope, $location, $interval) {
+function runBlock($window, $rootScope, $location, $timeout) {
   $rootScope.nowPlaying = {};
   $rootScope.podcasts = [];
 
-  Zeus.loadSettings(function (data) {
-    // Use the theme to determine if they have settings
-    if (data.theme === undefined) {
-      $rootScope.settings = {
-        theme: 'light',
-        animations:  true,
-        analytics:  true,
-        voiceBoost: true,
-        smartSpeed: true,
-        autoPlay:  true,
-        volume: 50,
-        cacheImages: true,
-        autoUpdate: true,
-        playback: 'download'
-      };
-    } else {
-      $rootScope.settings = data;
-    }
+  $timeout(function () {
+    Zeus.loadSettings(function (settings) {
+      // Use the theme to determine if they have settings
+      if (settings.theme === undefined) {
+        $rootScope.settings = {
+          theme: 'light',
+          animations:  true,
+          analytics:  true,
+          voiceBoost: true,
+          smartSpeed: true,
+          autoPlay:  true,
+          volume: 50,
+          cacheImages: true,
+          autoUpdate: true,
+          playback: 'download'
+        };
+      } else {
+        $rootScope.settings = settings;
+      }
 
-    Zeus.settings = $rootScope.settings;
-    Zeus.loadSavedPodcasts(function (data) {
-      console.log(data);
-      $rootScope.podcasts = data;
-      $rootScope.fullyLoaded = true;
+      Zeus.settings = $rootScope.settings;
+      console.log(settings);
+
+      Zeus.loadSavedPodcasts(function (data) {
+        console.log(data);
+        $rootScope.podcasts = data;
+        $rootScope.fullyLoaded = true;
+
+        $rootScope.$apply();
+      });
     });
-  });
+  }, 1000);
 }
